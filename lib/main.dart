2b6033a2d91b42c92c72dbe1958d317398521fc1
jude <justclassic24@gmail.com>
2024-07-12 06:00:25 +0100

    first push to github

--- lib/main.dart
+++ lib/main.dart
@@ -15,7 +15,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      title: 'Donitaa Client',
+      title: 'Diliverr',
       locale: const Locale('en'),
       localizationsDelegates: const [
         S.delegate,
